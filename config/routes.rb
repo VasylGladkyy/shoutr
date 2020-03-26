858a3cba56dfd7a8a12b3490ac0b884f658bff5c
Vasyl Gladkyy <vasil.gladkiy@gmail.com>
2020-03-26 22:59:19 +0200

    Allow user to shout

--- config/routes.rb
+++ config/routes.rb
@@ -17,4 +17,6 @@ Rails.application.routes.draw do
   get "/sign_in" => "sessions#new", as: "sign_in"
   delete "/sign_out" => "sessions#destroy", as: "sign_out"
   get "/sign_up" => "users#new", as: "sign_up"
+  
+  resources :shouts, only: :create
 end

fb7fe4e4079e850d70311f81d0b006423607072b
Vasyl Gladkyy <vasil.gladkiy@gmail.com>
2020-03-27 23:30:12 +0200

    Add Folowers reletionship

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   resource :session, only: [:create]
 
   resources :users, only: [:show, :create] do
+    resources :followers, only: :index
     member do
       post "follow", to: "followed_users#create"
       delete "unfollow", to: "followed_users#destroy"

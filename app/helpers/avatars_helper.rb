fed8434754dedb68a5ad6fcde6175cbf40e1504c
Vasyl Gladkyy <vasil.gladkiy@gmail.com>
2020-03-28 23:25:52 +0200

    Show shout on dashboard

--- app/helpers/avatars_helper.rb
+++ app/helpers/avatars_helper.rb
@@ -2,6 +2,6 @@ module AvatarsHelper
   def avatar(user)
     email_digest = Digest::MD5::hexdigest(user.email.downcase)
     gravatar_url = "//www.gravatar.com/avatar/#{email_digest}"
-    image_tag(gravatar_url)
+    image_tag(gravatar_url, class: "avatar")
   end
 end

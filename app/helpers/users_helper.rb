0f11ade47769ed01dc641d1044882528bb9fb60f
Vasyl Gladkyy <vasil.gladkiy@gmail.com>
2020-03-31 14:22:30 +0300

    Change user show page for Guest

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,15 @@
 module UsersHelper
   def follow_button(user)
-    if current_user.following?(user)
-      button_to "Unfollow", unfollow_user_path(user), method: :delete, class: "btn btn-primary ml-5 mt-3 mb-2"
-    else
-      button_to "Follow", follow_user_path(user), class: "btn btn-primary ml-5 mt-3 mb-2"
+    unless current_user.is_a?(Guest)
+      if current_user.following?(user)
+        button_to "Unfollow",
+                  unfollow_user_path(user),
+                  method: :delete, class: "btn btn-primary ml-5 mt-3"
+      else
+        button_to "Follow",
+                  follow_user_path(user),
+                  class: "btn btn-primary ml-5 mt-3"
+      end
     end
   end
 end
